620a6828c4eef1ff813c14c971ea9e2d9dc08248
Farhan Masud Aneek <farhan.aneek@gmail.com>
2022-03-12 16:02:27 +0600

    Sentry settings moved to staging and production from base

--- config/settings/base.py
+++ config/settings/base.py
@@ -169,24 +169,3 @@ TAILWIND_APP_NAME = "theme"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-# try for sentry variable on the .env file
-
-try:
-    sentry_sdk.init(
-        dsn=env("SENTRY_DSN"),
-        integrations=[DjangoIntegration()],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True,
-    )
-except:
-    pass
