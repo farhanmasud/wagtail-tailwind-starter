d56cdf15a6238334a6bd9b850d0beea5b3de2f81
Farhan Masud Aneek <farhan.aneek@gmail.com>
2022-09-09 01:45:10 +0600

    Debug reloading issue fixed

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [
+    urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
         path("__reload__/", include("django_browser_reload.urls")),
-    ]
+    ] + urlpatterns
